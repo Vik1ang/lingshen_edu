0539d3efa5d3ea22a50d3472c17f79d48517ea28
Vik1ang <vik1ang@outlook.com>
2022-04-11 19:35:48 +0800

    singleton

---
+++ 1.1.5_design_pattern/singleton/singleton1.cpp
@@ -0,0 +1,20 @@
+class Singleton {
+public:
+    static Singleton* GetInstance() {
+        if (_instance == nullptr) {
+            _instance = new Singleton();
+        }
+        return _instance;
+    }
+
+private:
+    Singleton() {} // 构造 为了保证全局只有一个
+    ~Singleton() {} // 避免直接被删除
+    Singleton(const Singleton& clone) {} // 拷贝构造, 禁止拷贝构造
+    Singleton& operator=(const Singleton&) {}
+    static Singleton* _instance;
+};
+
+int main() {
+    Singleton* Singleton::_instance = nullptr; // 静态成员需要初始化
+}
