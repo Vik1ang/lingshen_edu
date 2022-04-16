5dab56258ebc3c8392f224b7dceb43b738e897b6
Vik1ang <vik1ang@outlook.com>
2022-04-16 23:12:09 +0800

    cpp11

--- 2.1_cpp11/1-1-shared_from_this2.cpp
+++ 2.1_cpp11/1-1-shared_from_this2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <memory>
 
-class A 
+class A : public std::enable_shared_from_this<A>
 {
 public:
     std::shared_ptr<A> GetSelf() {
-        return std::shared_ptr<A>(this); // 不要这么操作
+        return shared_from_this();
     }
     ~A() {
         std::cout << "Deconstruction A" << std::endl;
@@ -15,7 +15,11 @@ public:
 int main() 
 {
     std::shared_ptr<A> sp1(new A);
-    std::shared_ptr<A> sp2 = sp1->GetSelf();
+    std::shared_ptr<A> sp2 = sp1->GetSelf(); // ok
+
+    std::cout << "sp1.use_count() = " << sp1.use_count()<< std::endl;
+    std::cout << "sp2.use_count() = " << sp2.use_count()<< std::endl;
+    std::cout << "leave {}" << std::endl;
 
     return 0;
 }
