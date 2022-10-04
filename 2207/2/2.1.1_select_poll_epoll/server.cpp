f653d6c9f568213037613980ddbec5c5b6e184dc
Vik1ang <vik1ang@outlook.com>
2022-10-04 09:12:56 +0800

    2

--- 2207/2/2.1.1_select_poll_epoll/server.cpp
+++ 2207/2/2.1.1_select_poll_epoll/server.cpp
@@ -4,26 +4,12 @@
 #include <unistd.h>
 
 #include <array>
+#include <cstring>
 #include <iostream>
 #include <thread>
 #include <vector>
 
 enum { BUFFER_LENGTH = 128 };
-
-class ThreadPool {
-    std::vector<std::thread> m_pool;
-
- public:
-    void push_back(std::thread thr) {
-        m_pool.push_back(std::move(thr));
-    }
-
-    ~ThreadPool() {                     // main 函数退出后会自动调用
-        for(auto &t: m_pool) t.join();  // 等待线程池全部执行完毕
-    }
-};
-
-ThreadPool t_pool;
 
 /*void* routine(void *arg){
     int client_fd = *static_cast<int*>(arg);
@@ -60,17 +46,29 @@ int main() {
     // 默认是阻塞的
     int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (listen_fd == -1) {
-        return -1;
+        std::cout << "create socket error: " << strerror(errno) << "errno"
+                  << errno << std::endl;
+        return 0;
     }
+
     struct sockaddr_in serv_addr {};
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr =
         htonl(INADDR_ANY);  // 其实对于0.0.0.0转不转没有所谓
     serv_addr.sin_port = htons(19999);  // 端口随便填
 
-    if (bind(listen_fd, reinterpret_cast<struct sockaddr *>(&serv_addr),
-             sizeof(serv_addr)) == -1) {
-        return -2;
+    if (bind(listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) ==
+        -1) {
+        std::cout << "bind socket error: " << strerror(errno) << "errno"
+                  << errno << std::endl;
+        return 0;
+    }
+
+    if (listen(listen_fd, 10) == -1) {
+        std::cout << "listen socket error: " << strerror(errno) << "errno"
+                  << errno << std::endl;
+        return 0;
     }
 
 #if 0
@@ -78,8 +76,6 @@ int main() {
     flag |= O_NONBLOCK;
     fcntl(listen_fd, F_SETFL, flag);
 #endif
-
-    listen(listen_fd, 10);
 
 #if 0
     //
@@ -99,14 +95,71 @@ int main() {
         //        std::cout << "ret : " << ret << std::endl;
     }
     return 0;
-#else
+#elif 0
     while (1) {
         struct sockaddr_in client {};
         socklen_t len = sizeof(client);
         int client_fd = accept(
             listen_fd, reinterpret_cast<struct sockaddr *>(&client), &len);
-        std::thread t([&] { my_routine(client_fd);});
+        std::thread t([&] { my_routine(client_fd); });
         t_pool.push_back(std::move(t));
     }
+#else
+
+    fd_set read_fds, write_fds, r_set, w_set;
+
+    FD_ZERO(&read_fds);
+    FD_ZERO(&write_fds);
+    FD_SET(listen_fd, &read_fds);
+
+    int max_fd = listen_fd;
+
+    while (1) {
+        r_set = read_fds;
+        w_set = write_fds;
+
+        int n_ready = select(max_fd + 1, &r_set, &w_set, nullptr, nullptr);
+        if (FD_ISSET(listen_fd, &r_set)) {
+            std::cout << "listen_fd --> " << std::endl;
+            struct sockaddr_in client;
+            socklen_t len = sizeof(client);
+            int client_fd = accept(listen_fd, (struct sockaddr *)&client, &len);
+            if (client_fd == -1) {
+                printf("accept socket error: %s(errno: %d)\n", strerror(errno),
+                       errno);
+                return 0;
+            }
+            FD_SET(client_fd, &read_fds);
+            if (client_fd > max_fd) {
+                max_fd = client_fd;
+            }
+            //            std::array<unsigned char, BUFFER_LENGTH> buffer = {0};
+            char buffer[BUFFER_LENGTH] = {0};
+            size_t ret = 0;
+            int i = 0;
+            for (i = listen_fd + 1; i <= max_fd; i++) {
+                if (FD_ISSET(i, &r_set)) {
+                    ret = recv(i, buffer, BUFFER_LENGTH, 0);
+                    std::cout << "buffer : " << buffer << ", ret : " << ret
+                              << std::endl;
+                    if (ret == 0) {
+                        FD_CLR(i, &read_fds);
+                        std::cout << "Disconnect" << std::endl;
+                        close(i);
+                    }
+                    //                    FD_SET(i, &write_fds);
+                    if (--n_ready == 0) {
+                        break;
+                    }
+                } else if (FD_ISSET(i, &w_set)) {
+                    ret = send(i, buffer, ret, 0);
+                }
+            }
+        }
+    }
+
 #endif
+
+    close(listen_fd);
+    return 0;
 }
